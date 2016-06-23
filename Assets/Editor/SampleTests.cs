21dc8f04532e502d4aed256d302682ee8667975d
Alex Emmet <aemmet@playtika.com>
2016-06-23 13:49:53 -0700

    Clearing up tabs.

--- Assets/Editor/SampleTests.cs
+++ Assets/Editor/SampleTests.cs
@@ -24,66 +24,66 @@ internal class SampleTests
         throw new Exception("Ignored this test");
     }
 
-	// aemmet - this is NOT recommended - your test case will still fail, and you'll have to start again
-	// alternately you're covering up an infrequent infinite loop. why?
-	[Test]
-	[Ignore("Ignored test")]
+    // aemmet - this is NOT recommended - your test case will still fail, and you'll have to start again
+    // alternately you're covering up an infrequent infinite loop. why?
+    [Test]
+    [Ignore("Ignored test")]
     [MaxTime(100)]
     [Category("Failing Tests")]
     public void SlowTest()
     {
         Thread.Sleep(200);
     }
-	
-	// aemmet - use this in internal cases where you want to early out the failure
-	[Test]
-	[Ignore("Ignored test")]
+    
+    // aemmet - use this in internal cases where you want to early out the failure
+    [Test]
+    [Ignore("Ignored test")]
     [Category("Failing Tests")]
     public void FailingTest()
     {
-		for (int i = 0; i < 4; i++)
-			if (i == 3)
-        		Assert.Fail();
-		Assert.Pass();
+        for (int i = 0; i < 4; i++)
+            if (i == 3)
+                Assert.Fail();
+        Assert.Pass();
     }
 
-	// aemmet - an inconclusive test Fails in Unity! This means that a Jenkins job 
-	// or other framework that looks at the Status Code of the execute command will fail!
-	[Test]
-	[Ignore("Ignored test")]
+    // aemmet - an inconclusive test Fails in Unity! This means that a Jenkins job 
+    // or other framework that looks at the Status Code of the execute command will fail!
+    [Test]
+    [Ignore("Ignored test")]
     [Category("Failing Tests")]
     public void InconclusiveTest()
     {
         Assert.Inconclusive();
     }
 
-	// aemmet - similarly you can early out a pass if future Assert scenarios would fail
+    // aemmet - similarly you can early out a pass if future Assert scenarios would fail
     [Test]
     public void PassingTest()
     {
-		int i = 1;
-		if (i > 0)
-        	Assert.Pass();
-		Assert.Fail();
+        int i = 1;
+        if (i > 0)
+            Assert.Pass();
+        Assert.Fail();
     }
 
     [Test]
     public void ParameterizedTest([Values(1, 2, 3)] int a)
     {
-		Assert.AreEqual(a.GetType(),typeof(int));
+        Assert.AreEqual(a.GetType(),typeof(int));
     }
 
     [Test]
     public void RangeTest([NUnit.Framework.Range(1, 10, 3)] int x)
     {
-		Assert.IsTrue(x % 3 == 1);
+        Assert.IsTrue(x % 3 == 1);
     }
 
     [Test]
     [Culture("pl-PL")]
     public void CultureSpecificTest()
-	{
-		Assert.Pass();
+    {
+        Assert.Pass();
     }
 
     [Test]
@@ -93,109 +93,109 @@ internal class SampleTests
         throw new ArgumentException("expected message");
     }
 
-	public enum Days : int { Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday }
+    public enum Days : int { Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday }
 
     [Theory]
-	public void DateChecking(Days day)
+    public void DateChecking(Days day)
     {
-		int next = ((int)day + 7) % 7;
-		Assert.AreEqual(day, (Days)next);
+        int next = ((int)day + 7) % 7;
+        Assert.AreEqual(day, (Days)next);
     }
 
-	[Test, Sequential]
-	public void DateValueTest(
-		[Values(Days.Sunday, Days.Monday, Days.Tuesday, Days.Wednesday, Days.Thursday, Days.Friday, Days.Saturday)] Days day,
-		[Values(0,1,2,3,4,5,6)] int value
-	)
-	{
-		Assert.AreEqual(day, (Days)value);
-		Assert.AreEqual(value, (int)day);
-	}
+    [Test, Sequential]
+    public void DateValueTest(
+        [Values(Days.Sunday, Days.Monday, Days.Tuesday, Days.Wednesday, Days.Thursday, Days.Friday, Days.Saturday)] Days day,
+        [Values(0,1,2,3,4,5,6)] int value
+    )
+    {
+        Assert.AreEqual(day, (Days)value);
+        Assert.AreEqual(value, (int)day);
+    }
 
-	[Datapoint]
-	static IList intList = new List<int>() { 1, 2, 3, 4 };
+    [Datapoint]
+    static IList intList = new List<int>() { 1, 2, 3, 4 };
 
-	[Datapoint]
-	static IList stringList = new List<string>() { "hello", "jello", "cello" };
+    [Datapoint]
+    static IList stringList = new List<string>() { "hello", "jello", "cello" };
 
-	[Theory]
-	public void TestListShuffle(IList input)
-	{
-		object[] elements = new object[input.Count];
-		input.CopyTo(elements, 0);
-		for (int i = 0; i < input.Count; ++i)
-		{
-			input.Shuffle();
-		}
-		for (int i = 0; i < elements.Length; ++i)
-		{
-			Assert.Contains(elements[i], input);
-		}
-	}
+    [Theory]
+    public void TestListShuffle(IList input)
+    {
+        object[] elements = new object[input.Count];
+        input.CopyTo(elements, 0);
+        for (int i = 0; i < input.Count; ++i)
+        {
+            input.Shuffle();
+        }
+        for (int i = 0; i < elements.Length; ++i)
+        {
+            Assert.Contains(elements[i], input);
+        }
+    }
 
-	[Datapoint]
-	Array intArray = new int[] { -1, -2, -3, -4, -5 };
+    [Datapoint]
+    Array intArray = new int[] { -1, -2, -3, -4, -5 };
 
-	[Datapoint]
-	Array stringArray = new string[] { "jam", "cam", "yam", "dam", "ram", "ham" };
+    [Datapoint]
+    Array stringArray = new string[] { "jam", "cam", "yam", "dam", "ram", "ham" };
 
-	[Theory]
-	public void TestTheoryArrayShuffle(Array input)
-	{
-		object[] elements = new object[input.Length];
-		input.CopyTo(elements, 0);
-		for (int i = 0; i < input.Length; ++i)
-		{
-			// aemmet - note that this doesn't require <T> for the Shuffle.
-			input.Shuffle();
-		}
-		for (int i = 0; i < elements.Length; ++i)
-		{
-			Assert.Contains(elements[i], input);
-		}
-	}
+    [Theory]
+    public void TestTheoryArrayShuffle(Array input)
+    {
+        object[] elements = new object[input.Length];
+        input.CopyTo(elements, 0);
+        for (int i = 0; i < input.Length; ++i)
+        {
+            // aemmet - note that this doesn't require <T> for the Shuffle.
+            input.Shuffle();
+        }
+        for (int i = 0; i < elements.Length; ++i)
+        {
+            Assert.Contains(elements[i], input);
+        }
+    }
 
-	[Test]
-	// aemmet - can't use a string[] in inputs, because NUnit separates the object[] elements, which then mismatch the method sig!
-	public void TestStringArrayShuffle()
-	{
-		string[] input = new string[] { "jam", "cam", "yam", "dam", "ram", "ham" };
-		TestArrayShuffle<string>(input);
-	}
+    [Test]
+    // aemmet - can't use a string[] in inputs, because NUnit separates the object[] elements, which then mismatch the method sig!
+    public void TestStringArrayShuffle()
+    {
+        string[] input = new string[] { "jam", "cam", "yam", "dam", "ram", "ham" };
+        TestArrayShuffle<string>(input);
+    }
 
-	[Test]
-	[TestCase(new int[] { -1, -2, -3, -4, -5 })] // aemmet - primitive type arrays can be used, since NUnit uses object[]
-	public void TestIntArrayShuffle(int[] input)
-	{
-		TestArrayShuffle<int>(input);
-	}
+    [Test]
+    [TestCase(new int[] { -1, -2, -3, -4, -5 })] // aemmet - primitive type arrays can be used, since NUnit uses object[]
+    public void TestIntArrayShuffle(int[] input)
+    {
+        TestArrayShuffle<int>(input);
+    }
 
-	public void TestArrayShuffle<T>(T[] input)
-	{
-		T[] elements = new T[input.Length];
-		input.CopyTo(elements, 0);
-		for (int i = 0; i < input.Length; ++i)
-		{
-			// aemmet - note that this doesn't require <T> for the Shuffle.
-			input.Shuffle();
-		}
-		for (int i = 0; i < elements.Length; ++i)
-		{
-			Assert.Contains(elements[i], input);
-		}
-	}
+    public void TestArrayShuffle<T>(T[] input)
+    {
+        T[] elements = new T[input.Length];
+        input.CopyTo(elements, 0);
+        for (int i = 0; i < input.Length; ++i)
+        {
+            // aemmet - note that this doesn't require <T> for the Shuffle.
+            input.Shuffle();
+        }
+        for (int i = 0; i < elements.Length; ++i)
+        {
+            Assert.Contains(elements[i], input);
+        }
+    }
 
-	[Test]
-	// aemmet - while this will not work outside of Unity, we can create game objects in our Scene in Unity Test Runs
-	// this will Dirty the current scene though!
-	public void GameObjectTest()
-	{
-		GameObject gameObject = new GameObject();
-		gameObject.AddComponent<CryptoRandom>();
+    [Test]
+    // aemmet - while this will not work outside of Unity, we can create game objects in our Scene in Unity Test Runs
+    // this will Dirty the current scene though!
+    public void GameObjectTest()
+    {
+        GameObject gameObject = new GameObject();
+        gameObject.AddComponent<CryptoRandom>();
 
-		string name = "Test";
-		gameObject.name = name;
+        string name = "Test";
+        gameObject.name = name;
 
-		Assert.AreEqual(name, gameObject.name);
-	}
+        Assert.AreEqual(name, gameObject.name);
+    }
 }
--- Assets/Source/Shuffle.cs
+++ Assets/Source/Shuffle.cs
@@ -2,29 +2,29 @@
 
 public static class Extensions
 {
-	static System.Random random = new System.Random();
-		
-	public static IList Shuffle(this IList collection)
-	{
-		for (int i = collection.Count - 1; i > 1; --i)
-		{
-			int swap = random.Next(0, i);
-			object temp = collection[swap];
-			collection[swap] = collection[i];
-			collection[i] = temp;
-		}
-		return collection;
-	}
+    static System.Random random = new System.Random();
+        
+    public static IList Shuffle(this IList collection)
+    {
+        for (int i = collection.Count - 1; i > 1; --i)
+        {
+            int swap = random.Next(0, i);
+            object temp = collection[swap];
+            collection[swap] = collection[i];
+            collection[i] = temp;
+        }
+        return collection;
+    }
 
-	public static T[] Shuffle<T>(this T[] collection)
-	{
-		for (int i = collection.Length - 1; i > 1; --i)
-		{
-			int swap = random.Next(0, i);
-			T temp = collection[swap];
-			collection[swap] = collection[i];
-			collection[i] = temp;
-		}
-		return collection;
-	}
+    public static T[] Shuffle<T>(this T[] collection)
+    {
+        for (int i = collection.Length - 1; i > 1; --i)
+        {
+            int swap = random.Next(0, i);
+            T temp = collection[swap];
+            collection[swap] = collection[i];
+            collection[i] = temp;
+        }
+        return collection;
+    }
 }
