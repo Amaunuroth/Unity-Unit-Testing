590e13e294e7c9ec0d274b1b7447867ff11e9f7e
Alex Emmet <aemmet@playtika.com>
2016-06-23 17:07:26 -0700

    Updating some assets to test random generation on WebGL & threads (commented out).

--- Assets/Source/CryptoRandom.cs
+++ Assets/Source/CryptoRandom.cs
@@ -1,15 +1,37 @@
 ﻿using UnityEngine;
+using System;
 using System.Collections;
+using System.Security.Cryptography;
 
-public class CryptoRandom : MonoBehaviour {
+public class CryptoRandom : MonoBehaviour
+{
+    private RNGCryptoServiceProvider rng;
+    public int Roll = 0;
+    public float Time = 1f;
+    public TextMesh Text;
+    byte[] data;
+    TestThread thread;
 
-    // Use this for initialization
-    void Start () {
-    
+    void Start ()
+    {
+        data = new byte[sizeof(Int32)];
+        rng = new RNGCryptoServiceProvider();
+//        thread = new TestThread();
+//        thread.StartThread();
+        StartCoroutine(MakeRoll());
     }
-    
-    // Update is called once per frame
-    void Update () {
-    
+
+    IEnumerator MakeRoll()
+    {
+        while (true)
+        {
+            rng.GetBytes(data);
+//            if (thread.Result > 0)
+//                Roll = thread.Result;
+//            else
+                Roll = BitConverter.ToInt32(data, 0);
+            Text.text = Roll.ToString();
+            yield return new WaitForSeconds(Time);
+        }
     }
 }
---
+++ Assets/Source/TestThreads.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Threading;
+using System.Collections.Generic;
+
+public class TestThread
+{
+    public int Result { get; private set; }
+
+    public void StartThread()
+    {
+        Debug.Log("emmet - thread started");
+        Thread thread = new Thread(RunUnityRandom);
+        thread.Start();
+    }
+
+    public void RunUnityRandom()
+    {
+        Debug.Log("emmet - threading?");
+    }
+}
